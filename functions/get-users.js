a360f617fd823386f2343d4be7340949ba2928ab
Jim Snelgrove <60110040+slevkalev@users.noreply.github.com>
2022-12-12 10:02:30 -0500

    adding on
    adding db connection

--- functions/get-users.js
+++ functions/get-users.js
@@ -1,8 +1,19 @@
+require("dotenv").config()
+const mysql = require("mysql2")
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
 exports.handler = async function () {
+  const sql = "SELECT * FROM `picks`"
+  const qResponse = connection.query(sql, (err, results) => {
+    if (err) {
+      res.send(err)
+    }
+    res.json(results)
+  })
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "gett database info",
+      message: qResponse,
     }),
   }
 }
